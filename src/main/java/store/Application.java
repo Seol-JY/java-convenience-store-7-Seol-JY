4366e1498b49b1356e1adafbdfd3eccbaf9c7acd
Seol_JY <wlsdud5654@gmail.com>
2024-11-09 17:34:10 +0900

    feat: promotions.md 파일 프로모션 정보 로드 기능 구현

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -2,11 +2,15 @@ package store;
 
 import java.util.List;
 import store.dto.ProductFileDto;
+import store.dto.PromotionFileDto;
 import store.loader.FileDataLoader;
 
 public class Application {
     public static void main(String[] args) {
         List<ProductFileDto> load = new FileDataLoader<>(ProductFileDto.class)
                 .load("src/main/resources/products.md");
+        List<PromotionFileDto> load2 = new FileDataLoader<>(PromotionFileDto.class)
+                .load("src/main/resources/promotions.md");
+        System.out.println(load2);
     }
 }
