f0a4e35efc8581a7ff859419d58fea0111cf10b0
Seol_JY <wlsdud5654@gmail.com>
2024-11-11 09:01:11 +0900

    fix(OutputView): Promotion 상품 정보가 있을때 일반 상품이 "재고 없음" 으로 표시되지 않는 문제 해결

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -12,6 +12,7 @@ public class OutputView {
     private static final String EMPTY_STRING = "";
     private static final String SPACE_STRING = " ";
     private static final String NEW_LINE = "\n";
+    private static final String ERROR_PREFIX = "[ERROR] ";
 
     public void printProducts(final List<ProductDto> products) {
         StringBuilder result = new StringBuilder();
@@ -24,11 +25,24 @@ public class OutputView {
         for (ProductDto product : products) {
             result.append(formatProduct(product))
                     .append(NEW_LINE);
+
+            if (product.promotion() != null && !containsNullPromotionWithSameName(products, product)) {
+                result.append(formatOutOfStockProduct(product))
+                        .append(NEW_LINE);
+            }
         }
 
         System.out.println(result);
     }
 
+    private boolean containsNullPromotionWithSameName(List<ProductDto> products, ProductDto targetProduct) {
+        return products.stream().anyMatch(product ->
+                product != targetProduct
+                        && product.name().equals(targetProduct.name())
+                        && product.promotion() == null
+        );
+    }
+    
     private String formatProduct(final ProductDto product) {
         return String.format(
                 PRODUCT_FORMAT,
@@ -36,6 +50,16 @@ public class OutputView {
                 product.price(),
                 formatStock(product.quantity()),
                 formatPromotion(product.promotion())
+        );
+    }
+
+    private String formatOutOfStockProduct(final ProductDto product) {
+        return String.format(
+                PRODUCT_FORMAT,
+                product.name(),
+                product.price(),
+                OUT_OF_STOCK,
+                EMPTY_STRING
         );
     }
 
@@ -53,5 +77,9 @@ public class OutputView {
         }
 
         return SPACE_STRING + promotion;
+    }
+
+    public void printError(String message) {
+        System.out.println(ERROR_PREFIX.concat(message));
     }
 }
