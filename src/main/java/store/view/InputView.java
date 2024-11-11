3fccfd0b29d2b270fadbac5e4d834a91e9d88018
Seol_JY <wlsdud5654@gmail.com>
2024-11-11 20:23:52 +0900

    fix(InputView): 개행 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -4,7 +4,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
     private static final String ORDER_INPUT_MESSAGE
-            = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
+            = "\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
     private static final String PROMOTION_ADDITIONAL_MESSAGE
             = "%n현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n";
     private static final String NORMAL_PRICE_CONFIRMATION_MESSAGE =
